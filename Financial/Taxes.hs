d376974f9d1ccdd0590f1b19e2046e32cbc151e4
Martin Bilski <martinb@Martin-Bilskis-MacBook-Air.local>
2011-10-26 21:44:48 +0200

    It looks that loss deduction works.

--- Financial/Taxes.hs
+++ Financial/Taxes.hs
@@ -1,6 +1,7 @@
 module Financial.Taxes where
 	
 	import Financial.Types (Currency, pln)
+	import Data.List (minimum)
 	
 	-- incomeTax :: [Currency] -> [Currency] -> (Currency, [Currency])
 	-- incomeTax profit
@@ -14,21 +15,44 @@ module Financial.Taxes where
 	-- 	where
 	-- 		step 
 
-	-- TODO Make periods configurable.
+	-- TODO: Make periods configurable.
+	-- TODO: Rename to deductLosses.
+	-- Profits -> profits and losses after loss deduction.
 	deduct :: [Currency] -> [Currency]
-	deduct = reverse . foldr (deduct2 5) [] . reverse
+	deduct = integrate . doDeduct . zipDupOf
+		where
+			integrate :: [(Currency, Currency)] -> [Currency]
+			integrate xs = map select xs
+			 where
+				select (orig, acc)	
+					| orig > 0 = acc	-- Profit: use calculation result.
+					| otherwise = orig	-- Loss: ignore calculation results, use original value.
 			
+	-- Profits -> profits interleaved with amounts of losses that couldn't be deducted.
+	deduct' :: [Currency] -> [Currency]
+	deduct' = snd . unzip . doDeduct . zipDupOf
+			
+	doDeduct :: [(Currency, Currency)] -> [(Currency, Currency)]
+	doDeduct = reverse . foldr (deduct2 5) [] . reverse
+
+	zipDupOf :: [a] -> [(a, a)]
+	zipDupOf xs = zip xs xs 
+		
 	-- TODO: No more than 50% of each loss but not the current one but the original ones!!
-	deduct2 :: Int -> Currency -> [Currency] -> [Currency]
+	deduct2 :: Int -> (Currency, Currency) -> [(Currency, Currency)] -> [(Currency, Currency)]
 	deduct2 deductiblePeriods crnt past = (foldr step [crnt] $ deductiblePast) ++ nonDeductiblePast
 		where
 			(deductiblePast, nonDeductiblePast) = splitAt deductiblePeriods past
-			step :: Currency -> [Currency] -> [Currency]
-			step x (x':xs') = (x' + dl) : (x - dl) : xs'
+			step :: (Currency, Currency) -> [(Currency, Currency)] -> [(Currency, Currency)]
+			step (orig, acc) ((orig', acc'):xs) = (orig', acc' + dl) : (orig, acc - dl) : xs
 				where
-					dl = deductibleLoss x' x
+					dl = deductibleLoss acc' acc orig
 
-	deductibleLoss :: Currency -> Currency -> Currency
-	deductibleLoss p l
-		| p > 0 && l < 0 = - (min p (- l / 2))
+	scanDeduct past = reverse $ scanr (deduct2 5) [] $ reverse $ zip past past
+
+	deductibleLoss :: Currency -> Currency -> Currency -> Currency
+	deductibleLoss currentProfit accumulatedLoss origLoss
+		| currentProfit > 0 && accumulatedLoss < 0 = - minimum [currentProfit, - origLoss / 2, - accumulatedLoss]
 		| otherwise = 0
+		
+-- TODO: Remove dist/ from git and ignore it.

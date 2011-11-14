64641a27ad5f02f5bfa0765f159186bebd18d76e
Martin Bilski <martinb@Martin-Bilskis-MacBook-Air.local>
2011-11-14 22:15:24 +0100

    We went on to implement story #1. The failing cucumber spec looks like it's done. We did the form, JS correctly (I think) POSTS to /report and then redirects to the returned URL.
    POST /report initializes project and right now does nothing more.
    
    All specs pass, cucumber tests pass except for @story1.

--- www/config/routes.rb
+++ www/config/routes.rb
@@ -1,4 +1,7 @@
 # Check out https://github.com/joshbuddy/http_router for more information on HttpRouter
 HttpRouter.new do
   add('/').to(HomeAction)
+  add('/report_form.html').to(ReportPageAction)
+  add('/report').post.to(GenerateReportAction)
 end
+
--- www/features/step_definitions/web_steps.rb
+++ www/features/step_definitions/web_steps.rb
@@ -2,6 +2,19 @@ When /^I visit the home page$/ do
   visit "/"
 end
 
+When /^the investor creates a new project "([^"]*)"$/ do |proj_name|
+  visit "/report_form.html"
+  find("input#project_name").set(proj_name)
+end
+
+When /^the investor generates a profitability report$/ do
+  find("#generate").click
+end
+
+Then /^the report should include "([^"]*)"$/ do |text|
+  page.should have_content(text)
+end
+
 Then /^it should say "([^"]*)"$/ do |text|
   page.should have_content(text)
 end
